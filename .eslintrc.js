00b1ed5257eb23a026814dbc3eac27ab1203c07c
oriolraventosc <oriol.raventos.c@gmail.com>
2022-10-10 20:32:23 +0200

    Add extends in eslint js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["standard-with-typescript", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
